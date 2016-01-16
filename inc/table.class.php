f04316aa6cac645ef4b05236844c55920c0704ee
arondius <info@webbb-online.nl>
2016-01-16 21:08:06 +0100

    Adding table rendering code

--- inc/table.class.php
+++ inc/table.class.php
@@ -4,4 +4,37 @@ require_once('base.class.php');
 
 class webbb_fotoreizen_table {
 
+	public function __construct() {
+		$this->register_shortcode();
+	}
+
+	public function register_shortcode() {
+		add_shortcode('fotoreizen_tabel', array($this, 'generate_table') );
+	}
+
+	public function generate_table() {
+		$output = 'table generation';
+		$base = new webbb_fotoreizen_base();
+		$fotoreizen = $base->get_fotoreizen();
+
+		$output .= '<table>';
+		$output .= '<tr>';
+		$output .= '<td>Reisdatum</td><td>Reis</td><td>Reiscode</td><td>Prijs</td><td>Beschibare Plaatsen</td><td>Vertrekgarantie</td>';
+		$output .= '</tr>';
+		foreach($fotoreizen as $fotoreis) {
+			$data = $base->get_travel_fields($fotoreis->ID)[$fotoreis->ID]['data'];
+			$output .= '<tr>';
+			$output .= '<td>' . $data['reisdatum_start'][1] . ' t/m ' . $data['reisdatum_eind'][1] . '</td>';
+			$output .= '<td><a href="' . post_permalink($fotoreis->ID) . '">' . $fotoreis->post_title . '</a></td>';
+			$output .= '<td>' . $data['reiscode'][1] . '</td>';
+			$output .= '<td>' . $data['prijs'][1] . '</td>';
+			$output .= '<td>' . $data['beschikbare_plaatsen'][1] . '</td>';
+			$output .= '<td>' . $data['vertrekgarantie'][1] . '</td>';
+			$output .= '</tr>';
+		}
+		$output .= '</table>';
+		return $output;
+	}
 }
+
+new webbb_fotoreizen_table();
