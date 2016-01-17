20c9808ab004e9b762a539fd12bc087a01b0f8da
arondius <info@webbb-online.nl>
2016-01-17 22:28:55 +0100

    Changing loop too accommodate for array structure

--- inc/table.class.php
+++ inc/table.class.php
@@ -20,17 +20,16 @@ class webbb_fotoreizen_table {
 		$output .= '<tr>';
 		$output .= '<td>Reisdatum</td><td>Reis</td><td>Reiscode</td><td>Prijs</td><td>Beschibare Plaatsen</td><td>Vertrekgarantie</td>';
 		$output .= '</tr>';
-		foreach($fotoreizen as $fotoreis) {
-			$data = $base->get_travel_fields($fotoreis->ID)[$fotoreis->ID]['data'];
-			$output .= '<tr>';
-			$output .= '<td>' . $data['reisdatum_start'][1] . ' t/m ' . $data['reisdatum_eind'][1] . '</td>';
-			$output .= '<td><a href="' . post_permalink($fotoreis->ID) . '">' . $fotoreis->post_title . '</a></td>';
-			$output .= '<td>' . $data['reiscode'][1] . '</td>';
-			$output .= '<td>' . $data['prijs'][1] . '</td>';
-			$output .= '<td>' . $data['beschikbare_plaatsen'][1] . '</td>';
-			$output .= '<td>' . $data['vertrekgarantie'][1] . '</td>';
-			$output .= '</tr>';
-		}
+		foreach($fotoreizen as $date) {
+				$output .= '<tr>';
+				$output .= '<td>' . $date['reisdatum_start'][1] . ' t/m ' . $date['reisdatum_eind'][1] . '</td>';
+				$output .= '<td><a href="' . post_permalink($fotoreis->ID) . '">' . $fotoreis->post_title . '</a></td>';
+				$output .= '<td>' . $date['reiscode'][1] . '</td>';
+				$output .= '<td>' . $date['prijs'][1] . '</td>';
+				$output .= '<td>' . $date['beschikbare_plaatsen'][1] . '</td>';
+				$output .= '<td>' . $date['vertrekgarantie'][1] . '</td>';
+				$output .= '</tr>';
+			}
 		$output .= '</table>';
 		return $output;
 	}
