084eea2c5a5e47dd50affab83f7d24487d10748c
arondius <info@webbb-online.nl>
2016-01-19 12:53:50 +0100

    Splitting array filling and table generation in separate methods

--- inc/table.class.php
+++ inc/table.class.php
@@ -12,24 +12,37 @@ class webbb_fotoreizen_table {
 		add_shortcode('fotoreizen_tabel', array($this, 'generate_table') );
 	}
 
-	public function generate_table() {
+	private function generate_array() {
 		$base = new webbb_fotoreizen_base();
 		$fotoreizen = $base->populate_fotoreizen_array();
+		$reisdatums = array();
+		foreach($fotoreizen as $fotoreis_id => $bestemming) {
+			foreach ($bestemming['reis'] as $date) {
+				foreach($date as $key => $date_entry) {
+					$reisdatum[$reiscode] = $date_entry[$key];
+				}
+			}
+		}
+		return $reisdatums;
+	}
+
+	public function generate_table() {
+		
 
 		$output .= '<table>';
 		$output .= '<tr>';
-		$output .= '<td>Reisdatum</td><td>Reis</td><td>Reiscode</td><td>Prijs</td><td>Beschibare Plaatsen</td><td>Vertrekgarantie</td>';
+		$output .= '<th>Reisdatum</th><th>Reis</th><th>Reiscode</th><th>Prijs</th><th>Beschibare Plaatsen</th><th>Vertrekgarantie</th>';
 		$output .= '</tr>';
-		foreach($fotoreizen as $date) {
-				$output .= '<tr>';
-				$output .= '<td>' . $date['reisdatum_start'][1] . ' t/m ' . $date['reisdatum_eind'][1] . '</td>';
-				$output .= '<td><a href="' . post_permalink($fotoreis->ID) . '">' . $fotoreis->post_title . '</a></td>';
-				$output .= '<td>' . $date['reiscode'][1] . '</td>';
-				$output .= '<td>' . $date['prijs'][1] . '</td>';
-				$output .= '<td>' . $date['beschikbare_plaatsen'][1] . '</td>';
-				$output .= '<td>' . $date['vertrekgarantie'][1] . '</td>';
-				$output .= '</tr>';
-			}
+		foreach($reisdatums as $reisdatum) {
+			$output .= '<tr>';
+			$output .= '<td>' . $reisdatum['reisdatum_start'][1] . ' t/m ' . $reisdatum['reisdatum_eind'][1] . '</td>';
+			$output .= '<td><a href="' . post_permalink($fotoreis_id) . '">' . $bestemming['title'] . '</a></td>';
+			$output .= '<td>' . $reisdatum['reiscode'][1] . '</td>';
+			$output .= '<td>' . $reisdatum['prijs'][1] . '</td>';
+			$output .= '<td>' . $reisdatum['beschikbare_plaatsen'][1] . '</td>';
+			$output .= '<td>' . $reisdatum['vertrekgarantie'][1] . '</td>';
+			$output .= '</tr>';
+		}
 		$output .= '</table>';
 		return $output;
 	}
